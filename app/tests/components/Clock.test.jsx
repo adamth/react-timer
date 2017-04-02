125dcdd91b3fb98718bb8a3b49cc75c3aa41fec1
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-02 20:31:29 +1000

    Add static controls element and tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -8,37 +8,37 @@ const Clock         = require('Clock');
 describe('Clock', () => {
     it('should exist', () => {
         expect(Clock).toExist();
-    })
-});
-
-describe('formatSeconds', () => {
-    it('should format seconds', () => {
-        var clock = testUtils.renderIntoDocument(<Clock />);
-
-        var seconds = 615;
-        var expected = '10:15';
-        var actual = clock.formatSeconds(seconds);
-
-        expect(actual).toBe(expected);
     });
 
-    it('should format seconds when min/sec are less than 10', () => {
-        var clock = testUtils.renderIntoDocument(<Clock />);
+    describe('formatSeconds', () => {
+        it('should format seconds', () => {
+            var clock = testUtils.renderIntoDocument(<Clock />);
 
-        var seconds = 61;
-        var expected = '01:01';
-        var actual = clock.formatSeconds(seconds);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
 
-        expect(actual).toBe(expected);
-    });
-}); 
+            expect(actual).toBe(expected);
+        });
 
-describe('Render', () => {
-    it('should render clock to output', () => {
-        var clock = testUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
-        var $el = $(ReactDOM.findDOMNode(clock));
-        var actualText = $el.find('.clock-text').text();
+        it('should format seconds when min/sec are less than 10', () => {
+            var clock = testUtils.renderIntoDocument(<Clock />);
 
-        expect(actualText).toBe('01:02');
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+    }); 
+
+    describe('Render', () => {
+        it('should render clock to output', () => {
+            var clock = testUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+
+            expect(actualText).toBe('01:02');
+        });
     });
 });
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -9,28 +9,28 @@ describe('Countdown', () => {
     it('should exist', () => {
         expect(Countdown).toExist();
     });
-});
 
-describe('handleSetCountdown', () => {
-    it('should set state to started and count down', (done) => {
-        var countdown = testUtils.renderIntoDocument(<Countdown />);
-        countdown.handleSetCountdown(10);
+    describe('handleSetCountdown', () => {
+        it('should set state to started and count down', (done) => {
+            var countdown = testUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(10);
 
-        expect(countdown.state.count).toBe(10);
-        expect(countdown.state.countdownStatus).toBe('started');
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
 
-        setTimeout(() => {
-            expect(countdown.state.count).toBe(9);
-            done();
-        }, 1001);      
-    });
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);      
+        });
 
-    it('should not count below 0', (done) => {
-        var countdown = testUtils.renderIntoDocument(<Countdown />);
-        countdown.handleSetCountdown(1);
-        setTimeout(() => {
-            expect(countdown.state.count).toBe(0);
-            done();
-        }, 2001);      
+        it('should not count below 0', (done) => {
+            var countdown = testUtils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(1);
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 2001);      
+        });
     });
 });
