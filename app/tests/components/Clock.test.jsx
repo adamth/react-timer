5c1ac08d5aa2b8f297a581e5d333280f3d3a8141
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-02 15:01:46 +1000

    Clock tests

---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,34 @@
+const React         = require('react');
+const ReactDOM      = require('react-dom');
+const expect        = require('expect');
+const $             = require('jQuery');
+const testUtils     = require('react-addons-test-utils');
+const Clock         = require('Clock');
+
+describe('Clock', () => {
+    it('should exist', () => {
+        expect(Clock).toExist();
+    })
+});
+
+describe('formatSeconds', () => {
+    it('should format seconds', () => {
+        var clock = testUtils.renderIntoDocument(<Clock />);
+
+        var seconds = 615;
+        var expected = '10:15';
+        var actual = clock.formatSeconds(seconds);
+
+        expect(actual).toBe(expected);
+    });
+
+    it('should format seconds when min/sec are less than 10', () => {
+        var clock = testUtils.renderIntoDocument(<Clock />);
+
+        var seconds = 61;
+        var expected = '01:01';
+        var actual = clock.formatSeconds(seconds);
+
+        expect(actual).toBe(expected);
+    });
+}); 
